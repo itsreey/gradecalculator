325cd24c78af8c7966aea13e6abe4fe0a4e3efd2
reena <reena@RYDEAPAD>
2025-04-13 12:43:33 +0800

    Added a "how to use" button, changed version number

--- gradecalculator/MainPage.xaml.cs
+++ gradecalculator/MainPage.xaml.cs
@@ -13,6 +13,12 @@ namespace gradecalculator
         {
             await Shell.Current.GoToAsync("CourseListPage");
         }
+
+        private void HelpBtn_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("How to Use", "In the course list page, you can add your courses taken so far, and calculate your overall GPA. Each course requires a name (enter anything you find memorable and easy to understand, it can be the subject code or even a shorthand), its credit amount and letter grade. Tap on a course to edit or delete it.", "Done");
+
+        }
     }
 
 }
